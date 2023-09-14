cc91283ff9d8639593103502e9fa483acef8675d
Gerry Lin <gerrylin200327@icloud.com>
2023-09-13 19:03:31 -0500

    sum of array method

---
+++ Assignment3/Assignment3.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <array>
+using namespace std;
+
+
+int sumArray(int arr[]) {
+    int sum = 0;
+        for (int i = 0; i < sizeof(arr); i++)
+             sum += arr[i];
+
+    return sum;
+}
+
+int main()
+{
+    int arr[] = { 5, 5, 10, 2};
+ 
+    cout << sumArray(arr) << endl;
+}
+
+
+
