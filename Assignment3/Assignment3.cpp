5073a54e63e7a97badef3070dc257fe6955c7222
Gerry Lin <gerrylin200327@icloud.com>
2023-09-13 19:24:25 -0500

    Product of Array

--- Assignment3/Assignment3.cpp
+++ Assignment3/Assignment3.cpp
@@ -14,11 +14,21 @@ int sumArray(int arr[]) {
     return sum;
 }
 
+
+int mulArray(int arr[]) {
+    int i, temp = 1;
+    for (i = 0; i < sizeof(arr); ++i) {
+        temp = temp * arr[i];
+    }
+    return temp;
+}
+
 int main()
 {
-    int arr[] = { 5, 5, 10, 2};
+    int arr[] = { 5, 5, 5, 5};
  
     cout << sumArray(arr) << endl;
+    cout << mulArray(arr) << endl;
 }
 
 
